e85682185318a9347d46b9ff82a0fcb59e06e936
Dem Demich <schi8m@gmail.com>
2026-08-09 20:21:27 +0300

    SupportsBlock

---
+++ src/ui/components/SupportBlock/index.tsx
@@ -0,0 +1,44 @@
+import type { JSX } from "react/jsx-runtime";
+import { StyledSupportBlockWrapper, StyledSupportCard, StyledSupportCards, StyledSupportCardSubtitle, StyledSupportCardText, StyledSupportCardTitle, StyledSupportImgBlock, StyledSupportInfoBlock, StyledSupportSubtitle, StyledSupportTitle } from "./styles";
+
+export interface ISupportCard {
+    icon: JSX.Element;
+    title: string;
+    subtitle: string;
+}
+
+export interface ISupportBlockProps {
+    title: string;
+    subtitle: string;
+    cards: ISupportCard[]
+}
+
+export const SupportBlock: React.FC<ISupportBlockProps> = ({
+    title = '',
+    subtitle = '',
+    cards = []
+}) => {
+    return (
+        <StyledSupportBlockWrapper>
+            <StyledSupportInfoBlock>
+                <StyledSupportTitle>{title}</StyledSupportTitle>
+                <StyledSupportSubtitle>{subtitle}</StyledSupportSubtitle>
+                <StyledSupportCards>
+                    {cards.map(c => <StyledSupportCard>
+                        {c?.icon}
+                        <StyledSupportCardText>
+                            <StyledSupportCardTitle>{c?.title}</StyledSupportCardTitle>
+                            <StyledSupportCardSubtitle>{c?.subtitle}</StyledSupportCardSubtitle>
+                        </StyledSupportCardText>
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12.175 9H0V7H12.175L6.575 1.4L8 0L16 8L8 16L6.575 14.6L12.175 9Z" fill="#6B8AC6"/>
+                        </svg>
+                    </StyledSupportCard>)}
+                </StyledSupportCards>
+            </StyledSupportInfoBlock>
+            <StyledSupportImgBlock>
+                <img />
+            </StyledSupportImgBlock>
+        </StyledSupportBlockWrapper>
+    )
+}
---
+++ src/ui/components/SupportBlock/styles.ts
@@ -0,0 +1,106 @@
+import styled from "styled-components";
+import { COLORS, SIZES } from "../../variables";
+import '@fontsource/akt';
+
+
+export const StyledSupportBlockWrapper = styled.div`
+    width: 100%;
+    display: flex;
+    gap: 27px;
+    justify-content: space-between;
+    align-items: center;
+    padding-inline: ${SIZES.INDENTS.DESKTOP_PADDINGS}px;
+    padding-block: 48px;
+    box-sizing: border-box;
+`
+
+export const StyledSupportInfoBlock = styled.div`
+    flex: 60;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 20px;
+`
+
+export const StyledSupportImgBlock = styled.div`
+    flex: 40;
+`
+
+export const StyledSupportTitle = styled.h1`
+    margin: 0;
+    padding: 0;
+
+    font-family: 'Akt', sans-serif;
+    font-weight: 500;
+    font-style: Medium;
+    font-size: 48px;
+    line-height: 48px;
+    letter-spacing: -1.2px;
+    vertical-align: middle;
+    text-transform: uppercase;
+    color: ${COLORS.MAIN.TEXT_LIGHT};
+`
+
+export const StyledSupportSubtitle = styled.span`
+    font-family: 'Roboto Condensed', sans-serif;
+    font-weight: 500;
+    font-style: Medium;
+    font-size: 18px;
+    line-height: 27px;
+    letter-spacing: 0px;
+    vertical-align: middle;
+    color: ${COLORS.SECONDARY.TEXT_DARK};
+    margin-top: 20px;
+`
+
+export const StyledSupportCards = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
+    width: 100%;
+`
+
+export const StyledSupportCard = styled.div`
+    padding: 32px;
+    border-radius: 16px;
+    background-color: #6B8AC633;
+    border: 1px solid #C4C6CF4D;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 32px;
+`
+
+export const StyledSupportCardText = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+`
+
+export const StyledSupportCardTitle = styled.h3`
+    margin: 0;
+    padding: 0;
+    font-family: 'Akt', sans-serif;
+    font-weight: 500;
+    font-style: Medium;
+    font-size: 22px;
+    line-height: 33px;
+    letter-spacing: -0.55px;
+    vertical-align: middle;
+    text-transform: uppercase;
+    color: ${COLORS.MAIN.TEXT_LIGHT};
+`
+
+export const StyledSupportCardSubtitle = styled.span`
+    font-family: 'Roboto Condensed', sans-serif;
+    font-weight: 500;
+    font-style: Medium;
+    font-size: 11px;
+    line-height: 13.2px;
+    letter-spacing: 0px;
+    vertical-align: middle;
+    text-transform: uppercase;
+    color: ${COLORS.SECONDARY.TEXT_DARK};
+`
+
